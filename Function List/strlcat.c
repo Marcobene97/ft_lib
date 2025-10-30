fb1f110bcbe8ec7364b4b6603081a7dbaa7dbaab
Marco Benedettelli <mbenedet@3-F-13.42heilbronn.de>
2025-10-30 17:22:08 +0100

    strlcat

--- Function List/strlcat.c
+++ Function List/strlcat.c
@@ -6,39 +6,81 @@
 /*   By: mbenedet <mbenedet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 18:44:15 by mbenedet          #+#    #+#             */
-/*   Updated: 2025/10/29 20:19:12 by mbenedet         ###   ########.fr       */
+/*   Updated: 2025/10/30 15:34:20 by mbenedet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
-#include <stdio.h>
 
  size_t ft_strlcat(char *dst, const char *src, size_t size)
  {
       size_t i;
       size_t j;
+      size_t k;
+      size_t space;
+
 
       i = 0;
       while(src[i] != '\0')
       {
         i++;
       }
-      size_t srcsize = i;
+      size_t srclen = i;
 
       j = 0;
-      while(dst[j] > size & j != size)
+      while(dst[j] != '\0' && j < size)
       {
         j++;
       }
-      size_t dstsize = j;
+      size_t dstlen = j;
 
-      if(dstsize >= size)
+      if(dstlen >= size)
       {
-        dst[j] = src[i] && dst[j];
+        return(size + srclen);
+      }
+      
+      space = size - dstlen - 1;
+      k = 0;
+      
+      while(k < space && src[k] != '\0')
+      {
+        dst[dstlen + k] = src[k];
+        k++;
       }
 
-      i = 0;
-      if(i <= size && dstsize > 0)
+      if(size > dstlen)
+      {
+        dst[dstlen + k] = '\0';
+      }
+      
+    return(dstlen + srclen);
+
+ }
+
+ /*
+ #include <stdio.h>
+ 
+ int main (void)
+ {
+    char dst[11] = "helloworld";
+    const char src[] = "thisisMecna";
+    size_t size = ft_strlcat(dst, src, 20);
+    printf("%zu\n", size);
+    printf("resulting string=%s\n", dst);
+    return(0);
+ }
+
+  1. Find how long dst already is (up to dstsize — to avoid going out of bounds).
+2. Find the length of src.
+3. If dstsize <= dst_len:
+     → return dstsize + src_len (because no concatenation happens).
+4. Otherwise:
+     → copy as many characters from src to the end of dst as will fit (dstsize - dst_len - 1).
+5. Add a '\0' terminator.
+6. Return dst_len + src_len. 
+
+i = 0;
+      if(i <= size && dstlen > 0)
       {
         dst[j] = dst[j] && src[i];
         i++;
@@ -57,25 +99,7 @@
     }
     dst[i] = '\0';  
     
-    return(dstsize + srcsize);
-
- }
-
- int main (void)
- {
-    char dst[12] = "helloworld";
-    const char src[11] = "thisisMecna";
-    size_t size = ft_strlcat(dst, src, 12);
-    printf("%zu\n", size);
-    return(0);
- }
 
 
-/*  1. Find how long dst already is (up to dstsize — to avoid going out of bounds).
-2. Find the length of src.
-3. If dstsize <= dst_len:
-     → return dstsize + src_len (because no concatenation happens).
-4. Otherwise:
-     → copy as many characters from src to the end of dst as will fit (dstsize - dst_len - 1).
-5. Add a '\0' terminator.
-6. Return dst_len + src_len. */
+
+*/
---
+++ Part 1.md
@@ -0,0 +1,11 @@
+
+Aim:
+
+you must reimplement a set of functions from the libc. Your version will have
+the same prototypes and behaviors as the originals, adhering strictly to their definitions
+in the man page
+
+Replicability:
+- Prototype
+- Behaviour
+
